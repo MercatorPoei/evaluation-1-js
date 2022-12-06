5e07ed679e908aa622c18e2f6ef8902a656bc8e0
Mercator <Mercator.poei@gmail.com>
2022-12-06 11:03:02 +0100

    Final Exercice 1

--- exercice-1/app.js
+++ exercice-1/app.js
@@ -1,2 +1,19 @@
-console.log('bonjour')
-console.log('bonjour')
+// declaration constant name
+const name = 'Riri';
+// affichage de name
+console.log(name);
+
+// declaration time
+let time = 10;
+// ajout +2 à time
+time = time + 2;
+// affichage de time dans console
+console.log(time);
+
+// declaration du boolean
+let done = false;
+// attribution de true a variable done
+done = true;
+// affichage de done dans console
+console.log(done);
+
