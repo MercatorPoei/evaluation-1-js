341605719864a920c4d13e10ae2aebbec7f7adf8
Mercator <Mercator.poei@gmail.com>
2022-12-06 12:01:45 +0100

    Final Exercice 4

--- exercice-4/app.js
+++ exercice-4/app.js
@@ -1,1 +1,36 @@
-console.log('bonjour')
+// Instanciation de la clasee Human
+class Human {
+    constructor(firstName,lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+const person = new Human('Donald','Duck');
+
+console.log('Bonjour '+person.firstName+' '+person.lastName);
+
+// Instanciation de la classe Child
+class Child extends Human {
+    constructor(firstName,lastName){
+
+        super(firstName,lastName);
+        
+        this.child = true;
+
+
+    }
+
+    play() {
+        console.log(`Je m'apelle ${this.firstName} et je suis entrain de jouer`);
+    }
+
+}
+
+//dclaration de riri
+const riri = new Child('Riri','Duck');
+
+console.log(riri);
+if (riri.child == true){
+    riri.play();
+}
