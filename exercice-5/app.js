a97e60ff55069a79382494d5e69853962cf679e0
Mercator <Mercator.poei@gmail.com>
2022-12-06 12:25:45 +0100

    Final Exercice 4

--- exercice-5/app.js
+++ exercice-5/app.js
@@ -1,1 +1,42 @@
-console.log('bonjour')
+let myArray= [];
+
+for(let i = 0; i < 5; i++){
+    myArray[i] = i+1;
+}
+
+console.table(myArray);
+
+
+for(let i = 0; i < 5; i++){
+    if (myArray[i] > 2){
+        console.log(myArray[i])
+    }
+}
+
+let myArrayWithObjects =  [
+    {
+        "id": 1,
+        "title": "toto"
+    },
+    {
+        "id": 2,
+        "title": "titi"
+    },
+    {
+        "id": 3,
+        "title": "toto"
+    },
+]
+
+
+myArrayWithObjects.push({"id":5,"title":"tztz"});
+console.log(myArrayWithObjects);
+
+
+myArrayWithObjects.unshift({"id":6,"title":"tztz"});
+console.log(myArrayWithObjects);
+
+console.log("Affichage de chaque ligne");
+for (let i of myArrayWithObjects){
+    console.log(`${i.id} ${i.title}`);
+}
