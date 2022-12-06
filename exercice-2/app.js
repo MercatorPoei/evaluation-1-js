d5a3a68c4a2c7f25ba2dfef23cd11942a72372c5
Mercator <Mercator.poei@gmail.com>
2022-12-06 11:28:26 +0100

    Final Exercice 2

--- exercice-2/app.js
+++ exercice-2/app.js
@@ -1,1 +1,34 @@
-console.log('bonjour')
+// Declaration fonction hello
+function hello(){
+    console.log('Hello World');
+}
+
+// appel de la fonction hello
+hello();
+
+// Declaration de la fonction helloSomeone
+function helloSomeone(name) {
+    console.log(`Bonjour ${name}`);
+}
+// appel avec parametre de helloSomeone
+helloSomeone('fifi');
+
+//Declaration de la fonction helloSomeoneUppercase
+function helloSomeoneInUppercase(name){
+    console.log('Bonjour '+ name.toUpperCase());
+}
+
+// appel avec parametre de helloSomeoneInUppercase
+helloSomeoneInUppercase('loulou');
+
+
+//Declaration de la fonction helloSomeoneInCapitalize
+function helloSomeoneInCapitalize(name){
+    let firstLetter = name[0].toUpperCase();
+    let sliceName = name.slice(1);
+    name = firstLetter + sliceName;
+    console.log(`Bonjour ${name}`);
+}
+
+// appel avec parametre de helloSomeoneInCapitalize
+helloSomeoneInCapitalize('picsou');
